850b38e17cd15feed25b9c31b436e4c96c1a337d
murtaza63 <murtaza63@gmail.com>
2022-05-07 17:45:01 +0500

    Initial Commit

---
+++ BooksSeller/BooksSellerApp.swift
@@ -0,0 +1,17 @@
+//
+//  BooksSellerApp.swift
+//  BooksSeller
+//
+//  Created by Ghulam Murtaza on 07/05/2022.
+//
+
+import SwiftUI
+
+@main
+struct BooksSellerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
